d81288a510c0f1ca75051b46d1bfa0e692ae0201
juliansangillo <44471531+juliansangillo@users.noreply.github.com>
2019-02-20 11:45:54 -0500

    Update #2

--- AI Search Proj/catAndMouse.java
+++ AI Search Proj/catAndMouse.java
@@ -2,9 +2,14 @@ public class catAndMouse {
 
     public static void main(String args[]) {
         
+        //Later, we will also allow us to input the size of the board,
+        //the start and goal of the mouse, and the cat positions.
+        //For now, we will keep it simple and use a 2 by 2 board until we get the search
+        //algorithms right.
+
         Board b = new Board();
 
-        b.position[1][0].hasACat = true;
+        b.position[1][0].hasACat = true;    //This is just temporary. It sets position 2 as a cat for now.
         b.print();
 
     }
@@ -62,7 +67,7 @@ class Pos {
 class Board {
 
     Pos[][] position;
-    Pos start, end;
+    Pos start, goal;
     
     Board() {
 
@@ -84,7 +89,7 @@ class Board {
                     position[i][j].setMoves(position[i][j - 1].label, position[i - 1][j].label);
         
         start = position[0][0];
-        end = position[1][1];
+        goal = position[1][1];
 
     }
 
@@ -110,8 +115,14 @@ class Board {
 
 }
 
-class Mouse {
+class UninformedMouse {
 
     
+
+}
+
+class InformedMouse {
+
+
 
 }
