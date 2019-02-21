2b069d511321815f941c1de349e9a133ac2a7978
juliansangillo <44471531+juliansangillo@users.noreply.github.com>
2019-02-21 14:04:30 -0500

    Update #5

--- AI Search Proj/catAndMouse.java
+++ AI Search Proj/catAndMouse.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class catAndMouse {
@@ -9,13 +10,43 @@ public class catAndMouse {
         //For now, we will keep it simple and use a 2 by 2 board until we get the search
         //algorithms right.
 
-        Board b = new Board();
-        UninformedMouse mouse = new UninformedMouse(b);
+        Scanner reader = new Scanner(System.in);
+        int length;
+        System.out.print("Enter the length of the board: ");
+        length = reader.nextInt();
+        System.out.print('\n');
+        Board b = new Board(length);
+        b.print();
 
+        int start, goal;
+        System.out.print("Enter the mouse starting position: ");
+        start = reader.nextInt();
+        System.out.print("Enter the goal position: ");
+        goal = reader.nextInt();
+
+
+        int cat;
+        System.out.println("Enter all positions with cats or -1 to stop.");
+        System.out.print("Cat: ");
+        cat = reader.nextInt();
+        while(cat != -1) {
+            b.setCat(cat);
+            System.out.print("Cat: ");
+            cat = reader.nextInt();
+        }
+        System.out.print('\n');
+        reader.close();
+
+        UninformedMouse mouse = new UninformedMouse(b);
         ArrayList path;
 
         b.print();
-        path = mouse.depthFirstSearch(0, 3);
+        System.out.print("Cat: ");
+        for(int i = 0; i < b.cat.size(); i++)
+            System.out.print(b.cat.get(i) + "     ");
+        System.out.print('\n');
+
+        path = mouse.depthFirstSearch(start, goal);
         mouse.printPath(path);
 
     }
@@ -25,45 +56,48 @@ public class catAndMouse {
 //Cat and Mouse Board Setup**********************************************************************
 class Board {
 
+    int length;
+
     int[][] position;
     ArrayList[] moves;
     
     ArrayList cat = new ArrayList<Integer>();
   
-    Board() {
+    Board(int length) {
 
-        position = new int[2][2];
-        moves = new ArrayList[2 * 2];
+        this.length = length;
 
-        for(int i = 0; i < 2; i++)
-            for(int j = 0; j < 2; j++)
-                position[i][j] = 2 * i + j;
+        position = new int[length][length];
+        moves = new ArrayList[length * length];
 
-        for(int i = 0; i < 2; i++)
-            for(int j = 0; j < 2; j++) {
-                moves[2 * i + j] = new ArrayList<Integer>();
-                try { moves[2 * i + j].add(position[i][j - 1]); } catch(Exception e) {};
-                try { moves[2 * i + j].add(position[i][j + 1]); } catch(Exception e) {};
-                try { moves[2 * i + j].add(position[i - 1][j]); } catch(Exception e) {};
-                try { moves[2 * i + j].add(position[i + 1][j]); } catch(Exception e) {};
+        for(int i = 0; i < length; i++)
+            for(int j = 0; j < length; j++)
+                position[i][j] = length * i + j;
+
+        for(int i = 0; i < length; i++)
+            for(int j = 0; j < length; j++) {
+                moves[length * i + j] = new ArrayList<Integer>();
+                try { moves[length * i + j].add(position[i][j - 1]); } catch(Exception e) {};
+                try { moves[length * i + j].add(position[i][j + 1]); } catch(Exception e) {};
+                try { moves[length * i + j].add(position[i - 1][j]); } catch(Exception e) {};
+                try { moves[length * i + j].add(position[i + 1][j]); } catch(Exception e) {};
             }
-
-        cat.add(2);                     //This is just temporary. It sets position 2 as a cat for now.
         
+    }
+
+    void setCat(int position) {
+
+        cat.add(position);
+
     }
 
     void print() {
         
-        for(int i = 0; i < 2; i++) {
-            for(int j = 0; j < 2; j++)
+        for(int i = 0; i < length; i++) {
+            for(int j = 0; j < length; j++)
                 System.out.print(position[i][j] + "   ");
             System.out.print('\n');
         }
-
-        System.out.print("Cats: ");
-        for(int i = 0; i < cat.size(); i++)
-            System.out.print(cat.get(i) + "     ");
-        System.out.print('\n');
 
     }
 
@@ -78,9 +112,9 @@ class Mouse {
     Mouse(Board b) {
 
         this.b = b;
-        visited = new Boolean[2 * 2];
+        visited = new Boolean[b.length * b.length];
 
-        for(int i = 0; i < 2 * 2; i++) {
+        for(int i = 0; i < b.length * b.length; i++) {
             visited[i] = false;
         }
 
@@ -93,6 +127,23 @@ class Mouse {
                 return true;
 
         return false;
+    }
+
+    void printPath(ArrayList path) {
+
+        if(path == null)
+            System.out.println("The mouse could not make it to the end alive!");
+        else {
+            System.out.print("Mouse: ");
+            for(int i = 0; i < path.size() - 1; i++)
+                System.out.print(path.get(i) + " -> ");
+            System.out.print(path.get(path.size() - 1) + "\n");
+
+            System.out.print('\n');
+            System.out.println("The mouse made it to the goal safely.");
+        }
+        System.out.print('\n');
+
     }
 
 }
@@ -112,38 +163,21 @@ class UninformedMouse extends Mouse {
         visited[current] = true;
         ArrayList neighbors = b.moves[current];
 
-        for(int i = 0; i < neighbors.size(); i++)
+        for(int i = 0; i < neighbors.size(); i++) {
             if((int)neighbors.get(i) != goal && !visited[(int)neighbors.get(i)] && !isCat((int)neighbors.get(i)))
                 path = depthFirstSearch((int)neighbors.get(i), goal);
             else if ((int)neighbors.get(i) == goal) {
                 path = new ArrayList<Integer>();
                 path.add(goal);
-                break;
             }
-
-        if(path == null) {
-            path = new ArrayList<Integer>();
-            path.add(-1);
+            if (path != null)
+                break;
         }
 
-        path.add(0, current);
+        if(path != null)    
+            path.add(0, current);
 
         return path;
-    }
-
-    void printPath(ArrayList path) {
-
-        System.out.print("Mouse: ");
-        for(int i = 0; i < path.size() - 1; i++)
-            System.out.print(path.get(i) + " -> ");
-        System.out.print(path.get(path.size() - 1) + "\n");
-
-        System.out.print('\n');
-        if((int)path.get(path.size() - 1) == -1)
-            System.out.println("The mouse could not make it to the end alive!");
-        else
-            System.out.println("The mouse made it to the goal safely.");
-
     }
 
 }
